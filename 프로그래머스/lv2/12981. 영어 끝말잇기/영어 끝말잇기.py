8238189668d8e13bfbb331e302af7ca0c4f11dbf
Jaehyun <37329009+jaehyun-ko@users.noreply.github.com>
2023-09-18 00:34:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,22 @@
+def dup_test(words:list):
+    for word in words:
+        tmp = words.copy()
+        tmp.remove(word)
+        try:
+            return tmp.index(word)+1
+        except:
+            tmp = words
+    return 0   
+
+def crash_test(words:list):
+    for i in range(len(words)-1):
+        if words[i][-1] != words[i+1][0]:
+            return i+1
+    return 0
+
+def solution(n, words):
+    t = max(dup_test(words), crash_test(words)) if not dup_test(words) * crash_test(words) else min(dup_test(words), crash_test(words))
+    # print(t)
+    if t > 0:
+        return [t%n+1, t//n+1]
+    return [0, 0]
